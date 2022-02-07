4a47df6ed701a3601956c06609269cef5a92bda2
JClair21 <jessicagoodling@gmail.com>
2022-02-06 22:36:25 -0500

    created HTML file

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+console.log(generatePage(name, github));
+
+        /* Name: ${userName}
+        Github: ${githubName}
+    `;
+};
+
+(userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(name, github);
+
 const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
@@ -13,4 +43,4 @@ profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
-
+*/
